da84afd85a04ffc04b130e501236bf6c30f2bbf1
pro-extremecoder <vladislavmustyatsa@gmail.com>
2021-01-23 20:44:40 +0200

    After adding caching

--- state_manager.py
+++ state_manager.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+from loguru import logger
 
 
 
@@ -11,26 +11,59 @@ class State:
 	def __getattribute__(self, attr):
 
 		if not MetaStateManager._check_if_attr_is_stored_data(attr):
-
 			return super().__getattribute__(attr)
 
-		state_dict = MetaStateManager._get_state_from_json_file()
-
-		return state_dict[attr]
+		return MetaStateManager._cache[attr]
 
 	def __setattr__(self, attr, value):
 
 		if not MetaStateManager._check_if_attr_is_stored_data(attr):
 			return super().__setattr__(attr, value)
 
-		state_dict = MetaStateManager._get_state_from_json_file()
+		cached_state = MetaStateManager._cache.get_cached_state().copy()
+		cached_state[attr] = value
 
-		state_dict[attr] = value
-
-		MetaStateManager._put_state_into_json_file(state_dict)
+		MetaStateManager._put_state_into_json_file(cached_state)
+		MetaStateManager._cache[attr] = value
+		
 
 	def __delattr__(self, attr):
 		raise TypeError("you can't remove attributes")
+
+class Cache:
+
+	'''class which stores state data in RAM'''
+
+	def __init__(self, state_dict):
+
+		'''Get dictionary object and record it in own __dict__'''
+
+		for key, value in state_dict.items():
+			self.__dict__[key] = value
+
+	def __setattr__(self, attr, value):
+
+		'''Set attributes given in __init__'''
+
+		if attr in self.__dict__:
+			self.__dict__[attr] = value
+		else:
+			raise AttributeError(f"type object 'Cache' has no attribute '{attr}'")
+
+	def __getitem__(self, item):
+		return self.__dict__[item]
+
+	def __setitem__(self, item, value):
+		if item in self.__dict__:
+			self.__dict__[item] = value
+		else:
+			raise KeyError(item)
+
+	def get_cached_state(self):
+
+		'''Return cached stored data dictionary'''
+
+		return self.__dict__
 
 
 class MetaStateManager(type):
@@ -38,6 +71,7 @@ class MetaStateManager(type):
 	'''metaclass which serves user state manager class'''
 
 	__state_manager = None
+	_cache = None
 	state = State()
 
 	def __init__(self, *args, **kwargs):
@@ -70,29 +104,40 @@ class MetaStateManager(type):
 			if data == "":
 				MetaStateManager._set_default_state()
 
+		MetaStateManager._cache = Cache(MetaStateManager._get_state_from_json_file())
+
+
 	def __state_manager_init(self, *args, **kwargs):
 
 		'''__init__ method for user state manager class'''
 		
 		raise TypeError(f"'{self.__class__.__name__}' can't have any instances")
 
-
-	@staticmethod
-	def _put_state_into_json_file(state_dict):
+	@classmethod
+	def _put_state_into_json_file(cls, state_dict):
 		
 		'''Get dictionary object as an arguement and put it into 'state.json' file'''
-
+		
 		with open('state.json', 'w') as file:
-			file.write(str(json.dumps(state_dict, sort_keys=True, indent=4)))
+			try:
+				file.write(str(json.dumps(state_dict, sort_keys=True, indent=4)))
+			except BaseException as e:
+				cls._put_state_into_json_file(cls._cache.get_cached_state())
+				raise e
 
-	@staticmethod
-	def _get_state_from_json_file():
+	@classmethod
+	def _get_state_from_json_file(cls):
 
 		'''Get state from 'state.json' file and return dictionary object of it'''
-		
-		with open('state.json', 'r') as file:
-			state_dict = json.load(file)
 
+
+		with open('state.json', 'r') as file:
+			try:
+				state_dict = json.load(file)
+			except BaseException as e:
+				cls._put_state_into_json_file(cls._cache.get_cached_state())
+				raise e
+		
 		return state_dict
 
 	@classmethod
@@ -124,7 +169,7 @@ class MetaStateManager(type):
 			key_error_raised = True
 		
 		if key_error_raised:
-			raise AttributeError(f"type object 'StateManager' has no attribute '{attr}'")
+			raise AttributeError(f"type object 'StoredDataSchema' has no attribute '{attr}'")
 
 		return False
 
